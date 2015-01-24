2cf3d7cb21c4b4310a4cdd96561194a5166ec69d
Josh Fornwall <josh@fornwall.com>
2015-01-24 00:21:45 -0500

    Initial checkin....

---
+++ src/main/java/com/fornsys/hana/config/MessageSecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package com.fornsys.hana.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MessageSecurityWebApplicationInitializer extends
+	AbstractSecurityWebApplicationInitializer {
+	
+	
+}
